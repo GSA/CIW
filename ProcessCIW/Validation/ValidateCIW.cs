4b0a515109270125e05a6181a62817cbd7ac3c38
Jason Goodell <jason.goodell@gsa.gov>
2017-07-20 09:52:29 -0400

    Add quote to log attempted values (#43)

--- ProcessCIW/Validation/ValidateCIW.cs
+++ ProcessCIW/Validation/ValidateCIW.cs
@@ -1348,7 +1348,7 @@ namespace ProcessCIW.Validation
                 //Print values only if not section 1 unless PropertyName is job title or part of name
                 if (section != 1 || rule.PropertyName == "PositionJobTitle" || rule.PropertyName == "LastName" || rule.PropertyName == "FirstName" || rule.PropertyName == "MiddleName")
                 {
-                    log.Error(string.Format("{0} failed with attempted value {1}", rule.PropertyName, String.IsNullOrWhiteSpace(rule.AttemptedValue.ToString()) ? "Empty" : rule.AttemptedValue));
+                    log.Error(string.Format("{0} failed with attempted value {1}", rule.PropertyName, String.IsNullOrWhiteSpace(rule.AttemptedValue.ToString()) ? "Empty" : '"' + rule.AttemptedValue.ToString() + '"'));
                 }
                 else 
                 {
