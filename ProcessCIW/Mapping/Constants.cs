b3877d4522b6b34b90c0204359497ad1e1aecb62
jgoodell-gsa <jason.goodell@gsa.gov>
2017-10-25 14:50:58 -0400

    Remove line 127 of constants. blank line

--- ProcessCIW/Mapping/Constants.cs
+++ ProcessCIW/Mapping/Constants.cs
@@ -124,6 +124,5 @@ namespace ProcessCIW.Mapping
         public const string CLICK_HERE_TO_ENTER_DATE = "Click here to enter a date.";
         public const string ENTER_DATE_OF_BIRTH = "Enter Date Of Birth";
         public const string CHOOSE_AN_ITEM = "Choose an item.";
-        
     }
 }
