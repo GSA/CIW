2a4ccd1e2bc8cedc74b785b56f73194103b253b5
YunZheng <YunZheng@DCOH2W-FXDFQ92>
2022-01-14 14:55:46 -0500

    update validation rule for contract not matching FAS or non-EASi contract nomenclature

--- ProcessCIW/Validation/ValidateCIW.cs
+++ ProcessCIW/Validation/ValidateCIW.cs
@@ -552,7 +552,7 @@ namespace ProcessCIW.Validation
                 When(b => (b.InvestigationTypeRequested.ToLower() != "tier 1c" && b.SponsoringOfficeSymbol.ToLower() != "pmc" && U.Utilities.validchildcare(b.TaskOrderDeliveryOrder) == false), () =>
                 {
 
-                    When(b => (U.Utilities.validFAS(b.TaskOrderDeliveryOrder) == false && U.Utilities.validcontractnumber(b.TaskOrderDeliveryOrder) == false && U.Utilities.validLeaseAndRandolphcontractnumber(b.TaskOrderDeliveryOrder) == false && b.SponsorIsPMCORCO.ToLower() == "pm"), () =>
+                    When(b => (U.Utilities.validFAS(b.TaskOrderDeliveryOrder) == false && U.Utilities.validcontractnumber(b.TaskOrderDeliveryOrder) == false && U.Utilities.validLeaseAndRandolphcontractnumber(b.TaskOrderDeliveryOrder) == false && MatchedEASiData(b.TaskOrderDeliveryOrder) == true), () =>
                     {
                         RuleFor(e => e.TaskOrderDeliveryOrder)
                             .Must(MatchedEASiData)
