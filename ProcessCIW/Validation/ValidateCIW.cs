2fc9d8610503a1beba12d671b9778ecd74add790
YunZheng <YunZheng@DCOH2W-FXDFQ92>
2022-02-04 09:22:11 -0500

    update rule to pass number which is existing non-easi and doesn't fit any of the approved nomenclatures

--- ProcessCIW/Validation/ValidateCIW.cs
+++ ProcessCIW/Validation/ValidateCIW.cs
@@ -560,7 +560,7 @@ namespace ProcessCIW.Validation
                 When(b => (b.InvestigationTypeRequested.ToLower() != "tier 1c" && b.SponsoringOfficeSymbol.ToLower() != "pmc" && U.Utilities.validchildcare(b.TaskOrderDeliveryOrder) == false), () =>
                 {
 
-                    When(b => (U.Utilities.validFAS(b.TaskOrderDeliveryOrder) == false && U.Utilities.validcontractnumber(b.TaskOrderDeliveryOrder) == false && U.Utilities.validLeaseAndRandolphcontractnumber(b.TaskOrderDeliveryOrder) == false), () =>
+                    When(b => (U.Utilities.validFAS(b.TaskOrderDeliveryOrder) == false && U.Utilities.validcontractnumber(b.TaskOrderDeliveryOrder) == false && U.Utilities.validLeaseAndRandolphcontractnumber(b.TaskOrderDeliveryOrder) == false && MatchedGCIMSData(b.TaskOrderDeliveryOrder) == false), () =>
                     {
                         RuleFor(e => e.TaskOrderDeliveryOrder)
                             .Must(MatchedEASiData)
