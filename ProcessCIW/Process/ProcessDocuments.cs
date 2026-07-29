1745b62daaa6f1e5458e88e6b394333ace785bf3
YunZheng <YunZheng@DCOH2W-FXDFQ92>
2026-07-29 11:39:37 -0400

    update validation rule for job title

--- ProcessCIW/Process/ProcessDocuments.cs
+++ ProcessCIW/Process/ProcessDocuments.cs
@@ -291,6 +291,11 @@ class ProcessDocuments
                     var citizenTag = xml.FirstChild.ChildNodes[2].ChildNodes[9].ChildNodes[4].ChildNodes[2].FirstChild.ChildNodes[1].Attributes[0].Value;
                     ciwInformation.Add(new CIWData { InnerText=citizenCountry, TagName=citizenTag+"2" });
 
+                    //get job title for test
+                    //var jobTitle = xml.FirstChild.ChildNodes[2].ChildNodes[7].ChildNodes[4].ChildNodes[2].LastChild.InnerText;
+                    //var jobtitleTag = xml.FirstChild.ChildNodes[2].ChildNodes[7].ChildNodes[4].ChildNodes[2].FirstChild.ChildNodes[1].Attributes[0].Value;
+                    //ciwInformation.Add(new CIWData { InnerText = jobTitle, TagName = jobtitleTag });
+
                     //get all table cells and add them after the version in ciwInformation
                     ciwInformation.AddRange( tableCells
                                         .Select
